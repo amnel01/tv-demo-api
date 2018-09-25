f492f44b74761510ae5f367375c8a63654d37409
Andrew Nelson <andrewmnelson06@gmail.com>
2018-09-24 19:12:51 -0600

    part 11

--- index.js
+++ index.js
@@ -2,26 +2,47 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
-app.get('/shows/:provider', (req, res) => {
-    const msg = `GET Show ${req.query.showName} from ${req.params.provider}`
-    console.log(msg)
-    res.send(msg)
+const tvShows = []
+
+// app.get('/shows/:provider', (req, res) => {
+//     const msg = `GET Show ${req.query.showName} from ${req.params.provider}`
+//     console.log(msg)
+//     res.send(msg)
+// })
+
+app.get('/shows', (req, res) => {
+    const title = req.query.showName
+    // console.log(title)
+    // res.send(JSON.stringify(tvShows))
+    console.log('saved shows:', tvShows)
+    const r = tvShows.filter((showDict) => {
+        return showDict.showName === title
+    })
+    res.send(r)
 })
 
 app.post('/shows', (req, res) => {
     const body = req.body
     console.log(body)
-    const name = req.body.showName
+    // console.log(body)
+    // const name = req.body
     // const name = body
     // res.send(`CREATE show ${req.body.showName}`)
-    res.send('CREATE show ' + name)
+    tvShows.push(body)
+    // res.send('CREATE show ' + JSON.stringify(body))
+    // console.log(tvShows)
+    res.send(tvShows)
 })
 
 app.put('/shows', (req, res) => res.send('UPDATE show'))
 
 app.delete('/shows', (req, res) => res.send('DELETE SHOW'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
