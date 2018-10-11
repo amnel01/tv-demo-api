73ccee343879daaa0b5e5047865f30de9540021a
Andrew Nelson <andrewmnelson06@gmail.com>
2018-10-10 22:22:27 -0600

    db intergation and validation

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3001
+const monk = require('monk')
+const Joi = require('joi');
+const dburl = "mongodb://adminTVDEMO:password1234@ds050077.mlab.com:50077/tv-demo-database"
+const db = monk(dburl)
+const tvShowsCollection = db.get('tvShow')
+
+const newTVShowSchema = Joi.object().keys({
+    name: Joi.string().required(),
+    rating: Joi.number().integer().min(1).max(5),
+    url: Joi.string().uri().required()
+})
+
+// validateTVShow = (req, res) => {
+//     const r = Joi.validate(req.body, newTVShowSchema)
+//     console.log(r)
+//     if (r.error === null)
+//         next()
+//     else 
+//         res.send(r.error)
+// }
+
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
+   })
+
+app.get('/shows', async (req, res) => {
+    try {
+        const tvShows = await tvShowsCollection.find({})
+        res.send(tvShows)
+    } catch(err) {
+        res.status(500).send(err.message)
+        // console.log(err)
+    }
 })
 
-const tvShows = []
-
-// app.get('/shows/:provider', (req, res) => {
-//     const msg = `GET Show ${req.query.showName} from ${req.params.provider}`
-//     console.log(msg)
-//     res.send(msg)
-// })
-
-app.get('/shows', (req, res) => {
-    const title = req.query.showName
-    // console.log(title)
-    // res.send(JSON.stringify(tvShows))
-    console.log('saved shows:', tvShows)
-    const r = tvShows.filter((showDict) => {
-        return showDict.showName === title
-    })
-    res.send(r)
-})
-
-app.post('/shows', (req, res) => {
-    const body = req.body
-    console.log(body)
-    // console.log(body)
-    // const name = req.body
-    // const name = body
-    // res.send(`CREATE show ${req.body.showName}`)
-    tvShows.push(body)
-    // res.send('CREATE show ' + JSON.stringify(body))
-    // console.log(tvShows)
-    res.send(tvShows)
+app.post('/shows', async (req, res) => {
+    const result = Joi.validate(req.body, newTVShowSchema)
+    if (result.error) {res.status(400).send(result.error)}
+    else {
+        try {
+            const body = req.body
+            const savedTVShow = await tvShowsCollection.insert(body)
+            res.send(savedTVShow)
+        } catch(err) {
+            res.status(500).send(err.message)
+        }
+    }
 })
 
 app.put('/shows', (req, res) => res.send('UPDATE show'))
